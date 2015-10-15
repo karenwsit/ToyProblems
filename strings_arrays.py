c789eeba3b6f17579e558b4c8343ceaf46291946
karenwsit <karenwsit@gmail.com>
2015-10-14 23:57:20 -0700

    finished permutation ex

--- strings_arrays.py
+++ strings_arrays.py
@@ -1,15 +1,28 @@
 ####### EX. 1.1 ########
 
-def unique_string(word):
+def is_unique_string(word):
     uchars = set()
     for c in word:
         if c in uchars:
             print False
-        uchars.add(c)
+        else:
+            uchars.add(c)
     return True
 
-# unique_string('tornado')
+# is_unique_string('tornado')
 
-###### EX. 1.2 ########
+####### EX. 1.2 ########
 
-def 
+def check_permutation(str1, str2):
+    if len(str1) != len(str2):
+        return False
+    str1 = str1.lower()
+    str2 = str2.lower()
+    if sorted(str1) == sorted(str2):
+        print True
+    else:
+        print False
+
+# check_permutation("god", "Dog")
+
+####### EX. 1.3 ########
