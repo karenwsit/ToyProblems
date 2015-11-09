b819e22d4cba38ab41696a51aff9df0323bc0668
karenwsit <karenwsit@gmail.com>
2015-11-09 13:02:36 -0800

    loop_detection added

---
+++ linked_lists/loop_detection.py
@@ -0,0 +1,25 @@
+#Exercise 2.8 of Cracking the Coding Interview
+#Given a circular linked list, implement an algorithm that returns the node at the beginning of the loop
+#################################################################################
+
+#Use Floyd's cycle-finding algorithm
+
+#Time = O(n)
+#Space = O(1)
+
+def loop_detection(ll):
+    if ll is None:
+        return False
+
+    slow = ll.head
+    fast = ll.head
+
+    if slow.next == None or fast.next == None:
+        return False
+
+    while fast != None:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            return True
+    return False
