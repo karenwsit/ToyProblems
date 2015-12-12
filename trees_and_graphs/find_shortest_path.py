e3a983ecbc0eb34e33abbf7809b38bc5bf17a99e
karenwsit <karenwsit@gmail.com>
2015-12-12 13:36:03 -0800

    reviewed find_all_paths.py and extended solution

---
+++ trees_and_graphs/find_shortest_path.py
@@ -0,0 +1,30 @@
+"""
+Variation of CTCI 4.1: (Find out whether or not there is a route between 2 nodes) Find the shortest path between 2 nodes
+"""
+
+def find_all_paths(graph, start, end, path=[]):
+    path += [start]
+
+    if start == end:
+        return path
+
+    if start not in graph:
+        return []
+
+    all_paths = []
+
+    for node in graph[start]:
+        if node not in path:
+            new_paths = find_shortest_path(graph, node, end, path)
+            for new_path in new_paths:
+                all_paths.append(new_path)
+
+    return all_paths
+
+def find_shortest_path(all_paths):
+
+    sorted_paths = sorted(all_paths)
+    return sorted_paths[0]
+
+def main():
+    find_shortest_path(find_all_paths())
