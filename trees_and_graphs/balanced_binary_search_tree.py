64120e2d45852df3e1c03eeb436caa840dd6ea6b
karenwsit <karenwsit@gmail.com>
2015-12-12 18:05:31 -0800

    first attempt at ssolving balanced_binary_search_tree.py

---
+++ trees_and_graphs/balanced_binary_search_tree.py
@@ -0,0 +1,18 @@
+"""
+CTCI 4.2: Given a sorted (increasing order) array with unique integer elements, write an alogirthm to create a binary search tree with minimal height
+"""
+
+def make_bst(nums, left, right):
+    node_index = len(nums)/2
+    node = nums[node_index]
+
+    left_subtree = nums[0:node_index]
+    right_subtree = nums[node_index:]
+
+    if len(left_subtree) == 1:
+        node.left = left_subtree[0]
+    if len(right_subtree) == 1:
+        node.right = right_subtree[0]
+
+    left = make_bst(left_subtree)
+    right = make_bst(right_subtree)
