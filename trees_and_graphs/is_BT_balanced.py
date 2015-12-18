43dd95440be0b0c3d09afab33c4883386a464577
karenwsit <karenwsit@gmail.com>
2015-12-17 18:56:53 -0800

    added recursive section to is_bt_balanced.py solution

---
+++ trees_and_graphs/is_BT_balanced.py
@@ -0,0 +1,14 @@
+"""
+CTCI 4.4: Implement a function to check if a binary tree is balanced. For the purposes of this question, a balanced tree is defined to be a tree such that the heights of the two node never differ by more than one
+"""
+
+def is_bt_balanced():
+
+    # Base Case
+    if node == None:
+        return True
+
+    if abs(depth(node.left) - depth(node.right)) > 1:
+        return False
+
+    return is_bt_balanced(node.left) and is_bt_balanced(node.right)
