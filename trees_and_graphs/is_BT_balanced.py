2bcdc9e4fa7f7f6e5a2ee13879c1b9bddd335d78
karenwsit <karenwsit@gmail.com>
2015-12-23 15:48:42 -0800

    debugged code to have the call the correct function

--- trees_and_graphs/is_BT_balanced.py
+++ trees_and_graphs/is_BT_balanced.py
@@ -10,54 +10,57 @@ class BinaryNode(object):
         self.left = left
         self.right = right
 
-def height(node):
+    def is_balanced(self):
+        """
+        >>> N = BinaryNode
+        >>> tree1 = N(1)
+        >>> tree1.is_balanced()
+        True
 
-    # Base Case
-    if node == None:
-        return 0
+        >>> tree2 = N(1, N(2))
+        >>> tree2.is_balanced()
+        True
 
-    return max(height(node.left), height(node.right)) + 1  # Unsure why we need to add 1
+        >>> tree3 = N(1, N(2), N(3))
+        >>> tree3.is_balanced()
+        True
+
+        >>> tree4 = N(1, N(2, N(3)), N(4))
+        >>> tree4.is_balanced()
+        True
+
+        >>> tree5 = N(1, N(2, N(3), N(4)), N(5))
+        >>> tree5.is_balanced()
+        True
+
+        >>> tree6 = N(1, N(2, N(3), N(4)))
+        >>> tree6.is_balanced()
+        False
+        """
+
+        def height(node):
+
+            # Base Case
+            if node is None:
+                return 0
+
+            return max(height(node.left), height(node.right)) + 1  # Unsure why we need to add 1
 
 
-def is_bt_balanced(node):
-    """
-    >>> N = BinaryNode
-    >>> tree1 = N(1)
-    >>> tree1.is_bt_balanced()
-    True
+        def is_bt_balanced(node):
 
-    >>> tree2 = N(1, N(2))
-    >>> tree2.is_bt_balanced()
-    True
+            # Base Case
+            if node is None:
+                return True
 
-    >>> tree3 = N(1, N(2), N(3))
-    >>> tree3.is_bt_balanced()
-    True
+            if abs(height(node.left) - height(node.right)) > 1:
+                return False
 
-    >>> tree4 = N(1, N(2, N(3)), N(4))
-    >>> tree4.is_bt_balanced()
-    True
-
-    >>> tree5 = N(1, N(2, N(3), N(4)), N(5))
-    >>> tree5.is_bt_balanced()
-    True
-
-    >>> tree6 = N(1, N(2, N(3), N(4)))
-    >>> tree6.is_bt_balanced()
-    False
-    """
-
-    # Base Case
-    if node == None:
-        return True
-
-    if abs(depth(node.left) - depth(node.right)) > 1:
-        return False
-
-    return is_bt_balanced(node.left) and is_bt_balanced(node.right)
+            return is_bt_balanced(node.left) and is_bt_balanced(node.right)
 
 
 if __name__ == '__main__':
     import doctest
+
     if doctest.testmod().failed == 0:
         print "\n*** ALL TESTS PASSED!\n"
