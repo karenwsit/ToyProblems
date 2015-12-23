e9ac734e1e527d252b8788b09c1242ff5da5e5f0
karenwsit <karenwsit@gmail.com>
2015-12-23 15:29:48 -0800

    added 6 test cases

--- trees_and_graphs/is_BT_balanced.py
+++ trees_and_graphs/is_BT_balanced.py
@@ -2,6 +2,14 @@
 CTCI 4.4: Implement a function to check if a binary tree is balanced. For the purposes of this question, a balanced tree is defined to be a tree such that the heights of the two node never differ by more than one
 """
 
+class BinaryNode(object):
+    """Node in binary tree."""
+
+    def __init__(self, data, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+
 def height(node):
 
     # Base Case
@@ -12,6 +20,42 @@ def height(node):
 
 
 def is_bt_balanced(node):
+    """
+    >>> N = BinaryNode
+    >>> tree1 = N(1)
+    >>> tree1.is_balanced()
+    True
+
+    >>> tree2 = N(1,
+                    N(2))
+    >>> tree2.is_balanced()
+    True
+
+    >>> tree3 = N(1,
+                    N(2), N(3))
+    >>> tree3.is_balanced()
+    True
+
+    >>> tree4 = N(1,
+                    N(2,
+                        N(3)),
+                    N(4))
+    >>> tree4.is_balanced()
+    True
+
+    >>>tree5 = N(1,
+                    N(2,
+                        N(3), N(4)),
+                    N(5))
+    >>>tree5.is_balanced()
+    True
+
+    >>>tree6 = N(1,
+                N(2,
+                    N(3), N(4)))
+    >>>tree6.is_balanced()
+    False
+    """
 
     # Base Case
     if node == None:
@@ -21,3 +65,9 @@ def is_bt_balanced(node):
         return False
 
     return is_bt_balanced(node.left) and is_bt_balanced(node.right)
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TESTS PASSED!\n"
