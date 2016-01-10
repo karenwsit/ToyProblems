68918a560eb82b46170eb38a81715d05f6fd0d4a
karenwsit <karenwsit@gmail.com>
2016-01-09 23:42:06 -0800

    solved problem. all tests passed

---
+++ strings_arrays/alternate_pos_neg.py
@@ -0,0 +1,40 @@
+def alternate_pos_neg(num_list):
+    """
+    Given a list of numbers, returns a list with alternating positive and negative 
+    numbers while maintaining order. If there are more positive numbers than negative
+    numbers, they appear at the end of the array, and vice versa.
+
+    >>> alternate_pos_neg([1, 2, 3, -4, -1, 4])
+    [1, -4, 2, -1, 3, 4]
+    >>> alternate_pos_neg([-5, -2, 5, 2, 4, 7, 1, 8, 0, -8])
+    [5, -5, 2, -2, 4, -8, 7, 1, 8, 0]
+    >>> alternate_pos_neg([-1, -2, 3, -4])
+    [3, -1, -2, -4]
+    """
+
+    pos_array = []
+    neg_array = []
+    result_array = []
+
+    for num in num_list:
+        if num < 0:
+            neg_array.append(num)
+        if num >= 0:
+            pos_array.append(num)
+
+    while len(pos_array) or len(neg_array) > 0:
+        if pos_array == []:
+            result_array.append(neg_array.pop(0))
+        elif neg_array == []:
+            result_array.append(pos_array.pop(0))
+        else:
+            result_array.append(pos_array.pop(0))
+            result_array.append(neg_array.pop(0))
+            
+    return result_array
+    
+########################################################################################
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
