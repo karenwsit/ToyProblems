e0e20ab62c01abf660e7ad75e104993ac86cb3b6
karenwsit <karenwsit@gmail.com>
2016-01-14 22:36:14 -0800

    completed onearraythreestacks implementation

--- stacks_and_queues/one_array_three_stacks.py
+++ stacks_and_queues/one_array_three_stacks.py
@@ -11,4 +11,29 @@ class SingleArrayStacks(object):
         self.pointer = [-1] * self.number  # [-1,-1,-1]
 
     def push(self, stacknum, value):
-        
+        if self.pointer[stacknum] + 1 >= self.stacksize:
+            print "Out of space"
+        else:
+            self.pointer[stacknum] += 1
+            self.array[self.stacktop(stacknum)] = value  
+
+    def pop(self, stacknum):
+        if self.pointer[stacknum] < 0:
+            return "Trying to pop an empty stack."
+        else:
+            data = self.array[self.stacktop(stacknum)]  
+            self.array[self.stacktop(stacknum)] = None
+            self.pointer[stacknum] -= 1
+            return data
+
+    def peek(self, stacknum):
+        if self.pointer[stacknum] < 0:
+            print "Empty stack"
+        else:
+            return self.array[self.stacktop(stacknum)]
+
+    def isEmpty(self, stacknum):
+        return self.pointer[stacknum] == -1
+
+    def stacktop(self, stacknum):
+        return self.stacksize * stacknum + self.pointer[stacknum]
