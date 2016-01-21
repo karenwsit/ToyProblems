391973f5a88852a6828a8e6110940140e2dfef71
karenwsit <karenwsit@gmail.com>
2016-01-20 20:56:12 -0800

    added recursive prase tree evaluate function

--- trees_and_graphs/parse_tree.py
+++ trees_and_graphs/parse_tree.py
@@ -31,3 +31,17 @@ def buildParseTree(fpexp):
 
 pt = buildParseTree("( ( 10 + 5 ) * 3 )")
 
+#Evaluating a Parse Tree
+
+def evaluate(parseTree):
+
+    opers = {'+': operator.add, '-': operator.sub, '*': operator.mul, '/': operator.truediv}
+
+    leftC = parseTree.getLeftChild()
+    rightC = parseTree.getRightChild()
+
+    if leftC and rightC:
+        fn = opers[parseTree.getRootVal()]
+        return fn(evaluate(leftC), evaluate(rightC))
+    else:
+        return parseTree.getRootVal()
