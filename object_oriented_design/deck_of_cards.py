de8fc67bc35d4a61dc5f9c9c2beaa6f82409f753
karenwsit <karenwsit@gmail.com>
2016-02-05 16:29:42 -0800

    completed data structure for implementation of generic deck of cards

---
+++ object_oriented_design/deck_of_cards.py
@@ -0,0 +1,28 @@
+#7.1 CTCI: Design the data structures for a generic deck of cars. Explain how you would subclass the data structures to implement blackjack.
+
+class Card(object):
+    suits = ('Clubs', 'Hearts', 'Spades', 'Diamonds')
+    pips = ('2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace')
+
+    def __init__(self, pip, suit):
+        self.pip = pip
+        self.suit = suit
+
+    def __str__(self):
+        return "%s %s" (self.pip, self.suit)
+
+
+class Deck(object):
+    def __init__(self):
+        self.deck = [Card(pip, suit)] for suit in Card.suits for pip in Card.pips]
+
+    def __str__(self):
+        return "[%s]"%", ".join( (str(card) for card in self.deck ))
+
+    def shuffle(self):
+        random.shuffle(self.deck)
+
+    def deal(self):
+        self.shuffle()
+        return self.deck.pop(0)
+
