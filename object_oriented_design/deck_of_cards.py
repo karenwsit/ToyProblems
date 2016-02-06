3258a669ee3c80f4f0f608578ecaa5776e90a64d
karenwsit <karenwsit@gmail.com>
2016-02-05 16:53:43 -0800

    added an add_card method

--- object_oriented_design/deck_of_cards.py
+++ object_oriented_design/deck_of_cards.py
@@ -1,5 +1,7 @@
 #7.1 CTCI: Design the data structures for a generic deck of cars. Explain how you would subclass the data structures to implement blackjack.
 
+import random
+
 class Card(object):
     suits = ('Clubs', 'Hearts', 'Spades', 'Diamonds')
     pips = ('2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace')
@@ -14,14 +16,22 @@ class Card(object):
 
 class Deck(object):
     def __init__(self):
-        self.deck = [Card(pip, suit)] for suit in Card.suits for pip in Card.pips]
+        self.cards = []
+        for suit in Card.suits:
+            for pip in Card.pips:
+                card = Card(pip, suit)
+                self.cards.append(card)
 
     def __str__(self):
-        return "[%s]"%", ".join( (str(card) for card in self.deck ))
+        result = [str(card) for card in self.cards]
+        return '\n'.join(result)
 
     def shuffle(self):
-        random.shuffle(self.deck)
+        random.shuffle(self.cards)
 
     def deal(self):
         self.shuffle()
-        return self.deck.pop(0)
+        return self.cards.pop(0)
+
+    def add_card(self, card):
+        self.cards.append(card)
