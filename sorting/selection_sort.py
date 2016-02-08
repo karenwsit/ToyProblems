7c783e73d5bacc486274755e0eba02d25613c1dd
karenwsit <karenwsit@gmail.com>
2016-02-08 14:48:21 -0800

    completed solution and all tests passed

--- sorting/selection_sort.py
+++ sorting/selection_sort.py
@@ -1,18 +1,19 @@
 #Selection Sort:
-def selection_sort(unsorted_array):
+def selection_sort(an_array):
     """
     >>> selection_sort([64, 25, 12, 22, 11])
-    [11, 12, 22, 25, 25, 64]
+    [11, 12, 22, 25, 64]
     """
-    sorted_array = []
 
-    for i in range(len(unsorted_array)+1):
-        minimum = unsorted_array[i]
-        if unsorted_array[i+1] < minimum:
-            minimum = unsorted_array[i+1]
-        sorted_array.append(minimum)
-    return sorted_array
-
+    for i in range(len(an_array)):
+        min_index = i
+        for j in range(i+1,len(an_array)):
+            if an_array[j] < an_array[min_index]:
+                min_index = j
+        temp = an_array[i]
+        an_array[i] = an_array[min_index]
+        an_array[min_index] = temp
+    return an_array
 
 
 if __name__ == "__main__":
