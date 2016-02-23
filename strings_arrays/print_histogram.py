e27a8eede662d087270571a8b4c0e974f93d61e0
karenwsit <karenwsit@gmail.com>
2016-02-22 20:40:07 -0800

    added vertical solution

--- strings_arrays/print_histogram.py
+++ strings_arrays/print_histogram.py
@@ -2,9 +2,9 @@
 
 # "test me there"
 
-#****
-#**
-#*****
+# ****
+# **
+# *****
 
 #   *
 # * *
@@ -12,10 +12,32 @@
 # ***
 # ***
 
-
 def print_histogram_reg(string):
+    """
+    """
+
     string_list = string.split()
     for word in string_list:
         print len(word) * '*'
 
 # print_histogram_reg('test me here')
+
+def print_histogram_ver(string):
+    """
+    """
+    string_list = string.split()
+    # print string_list
+    max_length = len(max(string_list, key=len))  # row num
+
+    new_string_list = []
+
+    for word in string_list:
+        new_string_list.append(len(word) * '*' + (max_length-len(word)) * '+')
+
+    for j in range(max_length-1, 0, -1):
+        for i in range(len(string_list)):
+            print new_string_list[i][j]
+
+    # Need to print new lines correctly; will refactor later
+
+print_histogram_ver('test me there')
