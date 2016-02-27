8b852e44c598a04f03653756e1688472fddcf140
karenwsit <karenwsit@gmail.com>
2016-02-26 19:22:08 -0800

    added simple solution

---
+++ strings_arrays/reverse_array_special.py
@@ -0,0 +1,31 @@
+#http://www.geeksforgeeks.org/reverse-an-array-without-affecting-special-characters/
+
+def reverse_string_special(string):
+    """
+    >>> reverse_string_special('a,b$c')
+    'c,b$a'
+    >>> reverse_string_special("Ab,c,de!$")
+    'ed,c,bA!$'
+    """
+    input_array = list(string)
+
+    char_array = [',', '$', '!']
+    temp_array = []
+
+    for i in range(len(input_array)):
+        if string[i] not in char_array:  # a special character
+            temp_array.append(input_array[i])
+
+    temp_array = temp_array[::-1]
+
+    for i in range(len(input_array)):
+        if input_array[i] not in char_array:
+            new_element = temp_array.pop(0)
+            input_array[i] = new_element
+
+    return "".join(input_array)
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
