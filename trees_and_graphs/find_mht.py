f21ff17c1f8641a63274acaba4a642ba9cc785fc
karenwsit <karenwsit@gmail.com>
2016-02-26 17:52:54 -0800

    finishd solution bfs

--- trees_and_graphs/find_mht.py
+++ trees_and_graphs/find_mht.py
@@ -19,7 +19,18 @@ def find_mht(n=None, edges=None):
         adj[i].add(j)
         adj[j].add(i)
 
-    print adj
+    leaves = [i for i in xrange(n) if len(adj[i]) == 1]
+
+    while n > 2:
+        n -= len(leaves)
+        new_leaves = []
+        for i in leaves:
+            j = adj[i].pop()
+            adj[j].remove(i)
+            if len(adj[j]) == 1:
+                new_leaves.append(j)
+
+    return leaves
 
 
 find_mht(n=4, edges=[[1, 0], [1, 2], [1, 3]])
