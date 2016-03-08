2c7710c74f4243bacfaf00bb1d1264a0fe8f2fb1
karenwsit <karenwsit@gmail.com>
2016-03-07 18:20:00 -0800

    added insertion sorty sol

---
+++ sorting/insertion_sort.py
@@ -0,0 +1,18 @@
+# Insertion Sort
+# Runtime: O(n^2)
+# Space: O(1)
+
+
+def insertion_sort():
+    
+    for index in range(1, len(alist)):
+        currentval = alist[index]
+        position = index
+
+        while position > 0 and alist[position-1] > currentval:
+            alist[position] = alist[position-1]
+            position = position - 1
+
+        alist[position]=currentval
+
+    return alist
