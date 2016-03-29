c3de2e5101336fb1ab08a6c7609e421a9b963512
karenwsit <karenwsit@gmail.com>
2016-03-29 16:49:54 -0700

    added example and assumption

--- strings_arrays/red_blue_blue_red.py
+++ strings_arrays/red_blue_blue_red.py
@@ -1,7 +1,7 @@
 """
 Complete the function below. Question was "Given a pattern and a string input - find if the string follows the same pattern and return 0 or 1. Examples: 1) Pattern : "abba", input: "redbluebluered" should return 1. 2) Pattern: "aaaa", input: "asdasdasdasd" should return 1. 3) Pattern: "aabb", input: "xyzabcxzyabc" should return 0.
+Assumption: new_input looks like ['red','blue','red','blue']
 """
-
 
 def  wordpattern( pattern,  input):
     #input_list = []
