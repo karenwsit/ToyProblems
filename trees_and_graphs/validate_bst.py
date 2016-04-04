6fb75be8194607e18791aeecd77ffe0b6a713118
karenwsit <karenwsit@gmail.com>
2016-04-04 16:09:08 -0700

    fixed bug. all tests pass

--- trees_and_graphs/validate_bst.py
+++ trees_and_graphs/validate_bst.py
@@ -2,7 +2,7 @@
 #Implement a function to check if a binary tree is a binary search tree
 #################################################################################
 
-class BinaryTree(object):
+class Node(object):
     def __init__(self, value):
         self.value = value
         self.left = None
@@ -18,9 +18,25 @@ def in_order_search(node, sorted_array=None):
     if sorted_array is None:
         sorted_array = []
     if node is None:
-        return []
-    while node:
-        in_order_search(node.left)
-        sorted_array = sorted_array.append(node.value)
-        in_order_search(node.right)
+        return False
+
+    in_order_search(node.left, sorted_array)
+    sorted_array.append(node.value)
+    in_order_search(node.right, sorted_array)
+
     return sorted_array
+
+    # if sorted_array[-1] > node.value:
+    #     return False
+
+#TESTING
+root = Node(4)
+root.left = Node(2)
+root.right = Node(5)
+root.left.left = Node(0)
+root.left.right = Node(3)
+root.left.left.right = Node(1)
+root.right.right = Node(0)
+
+print in_order_search(root)
+print is_bst(root)
