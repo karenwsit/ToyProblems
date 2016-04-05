f978ba859808a99b88db4be6382f25d7ec5b6de2
karenwsit <karenwsit@gmail.com>
2016-04-05 16:37:25 -0700

    added second solution that returns a list instead of printing

--- recursion/permutation.py
+++ recursion/permutation.py
@@ -5,7 +5,7 @@ l = starting index
 r = ending index
 """
 #Runtime: O(n*n!)
-
+#Solution will printe duplicate permutations if there are repeating characters
 def permute(a, l, r):
     """
     >>> permute(["J","O","N"], 0, 2)
@@ -23,6 +23,22 @@ def permute(a, l, r):
             a[l], a[i] = a[i], a[l] # swap
             permute(a, l+1, r)
             a[l], a[i] = a[i], a[l] # backtrack
+
+#This solution saves the results in a list
+def permute2(s):
+    """
+    >>> permute2('abc')
+    ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
+    """
+    res = []
+    if len(s) == 1:
+        res = [s]
+
+    for i, c in enumerate(s):
+        for perm in permute2(s[:i] + s[i+1:]):
+            res += [c+perm]
+
+    return res
 
 if __name__ == "__main__":
     import doctest
--- stacks_and_queues/particle_count.py
+++ stacks_and_queues/particle_count.py
@@ -5,7 +5,7 @@ import re
 
 #With Regex
 def count_particles(string):
-    alist = re.split(r'\(|\)', string) 
+    alist = re.split(r'\(|\)', string)
     #['', '', 'H2O', '300', 'AM4B', '6', '2000']
 
     new_list = []
@@ -17,19 +17,18 @@ def count_particles(string):
             new_list.append(int(element))
         else:
             new_list.append(element)
-    
-    # Want:  ['H2O', 300, 'AM4B', 6, 2000]
 
-#Without Regex
-def count_particles(string):
+    #new_list = ['H2O', 300, 'AM4B', 6, 2000]
+    stack = []
+    count = 0
+
+    #is this block of code necessary? Maybe I can parse it better with regex
     for element in new_list:
         if isinstance(element, string):
             for char in element:
                 if char.isdigit():
 
-    string_list = string.split()
-    stack = []
-    count = 0
+
     for char in string:
         if char == '(':
             stack.append(char)
@@ -44,7 +43,7 @@ def count_particles(string):
 
     return count_particles
 
-count_particles(r'((H2O)300(AM4B)6)2000')
+print count_particles(r'((H2O)300(AM4B)6)2000')
 
 # ( (H2O)300)
 
