dafabf0960cb1da430f54a288af6148cbeacdbbd
karenwsit <karenwsit@gmail.com>
2016-04-04 21:31:06 -0700

    binary search recursive solution

--- sorting/binary_search.py
+++ sorting/binary_search.py
@@ -1,11 +1,23 @@
-#Binary Search: Return its location
-#Runtime: O(n)
+#Binary Search
+#Runtime: O(logn)
 
 def binary_search(sorted_array, x):
+    """
+    >>> binary_search([1,2,3,4,5], 1)
+    True
+    >>> binary_search([1,2,3,4,5], 6)
+    False
+    """
+    if sorted_array == []:
+        return False
+    mid = len(sorted_array) // 2
+    if x == sorted_array[mid]:
+        return True
+    elif x < sorted_array[mid]:
+        return binary_search(sorted_array[:mid], x)
+    elif x > sorted_array[mid]:
+        return binary_search(sorted_array[mid+1:], x)
 
-    for i in range(len(sorted_array)):
-        if sorted_array[i] == x:
-            return i
-    return -1
-
-
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
