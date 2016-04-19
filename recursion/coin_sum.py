41ab66688abcb648cfe81df7fc44f68cd14c537d
karenwsit <karenwsit@gmail.com>
2016-04-18 20:10:01 -0700

    working solution

--- recursion/coin_sum.py
+++ recursion/coin_sum.py
@@ -2,25 +2,26 @@
 """
 
 def coin_sum(target):
+    index = 0
     summ = 0
     res = []
     coins = [1, 2, 5, 10, 20, 50, 100, 200]
     curr_combo = []
-    traverse(coins, target, summ, res, curr_combo)
+    traverse(coins, target, index, summ, res, curr_combo)
     return res
 
 
-def traverse(coins, target, summ, res, curr_combo):
+def traverse(coins, target, index, summ, res, curr_combo):
     if summ == target:
-        res.append(curr_combo)
+        res.append(list(curr_combo))
+        return
     else:
-        for i in range(len(coins)):
+        for i in range(index, len(coins)):
             if summ + coins[i] > target:
                 break
             else:
-                summ += coins[i]
                 curr_combo.append(coins[i])
-                traverse(coins, target, summ, res, curr_combo)
-    return res
+                traverse(coins, target, i, summ+coins[i], res, curr_combo)
+                curr_combo.pop()
 
 print coin_sum(5)
