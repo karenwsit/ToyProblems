7c847d853fc0e5157859410012cd26b2a022cb64
karenwsit <karenwsit@gmail.com>
2016-04-20 14:25:24 -0700

    added infix to postfix expr

--- stacks_and_queues/eval_chem_exp.py
+++ stacks_and_queues/eval_chem_exp.py
@@ -22,9 +22,44 @@ def infix(expr):
         else:
             output_list.append(input_list[i])
         i += 1
-    return "".join(output_list)
+    return output_list
     #['(', '(', 'H2O', ')', '*', '300', '+', '(', 'AM4B', ')', '*', '6', ')', '*', '2000']
 
-# def postfix(expr2):
+def postfix(expr2):
+    print expr2
+    prec = {
+        '*': 3,
+        '/': 3,
+        '+': 2,
+        '-': 2,
+        '(': 1
+    }
 
-print infix('( ( H2O ) 300 ( AM4B ) 6 ) 2000')
+    op_stack = []
+    postfix_list = []
+    for token in expr2:
+        if token not in prec and token != ')':
+            postfix_list.append(token)
+        elif token == '(':
+            op_stack.append(token)
+        elif token == ')':
+            top_token = op_stack.pop()
+            while top_token != '(':
+                postfix_list.append(top_token)
+                top_token = op_stack.pop()
+        else:
+            while len(op_stack) != 0 and (prec[op_stack[-1]] >= prec[token]):
+                postfix_list.append(op_stack.pop())
+            op_stack.append(token)
+
+    while len(op_stack) != 0:
+        postfix_list.append(op_stack.pop())
+
+    return postfix_list  #['H2O', '300', '*', 'AM4B', '6', '*', '+', '2000', '*']
+
+
+def eval(expr3):
+ 
+
+expr2 = infix('( ( H2O ) 300 ( AM4B ) 6 ) 2000')
+postfix(expr2)
