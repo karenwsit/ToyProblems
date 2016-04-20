6fa431d3e4204c706c1eaa150ae6d1b655284ef9
karenwsit <karenwsit@gmail.com>
2016-04-19 21:13:51 -0700

    finished solution

--- other_concepts/query_relevance.py
+++ other_concepts/query_relevance.py
@@ -12,8 +12,8 @@ def build_dicts(query, corpus):
     total_doc_num = 0
 
     for doc in corpus:
-        tf_dict[doc] = tf_dict.get(doc, {})  #builds out nested dic for every doc in corpus
-        total_doc_num += 1   #keeps track of # of docs in the corpus
+        tf_dict[doc] = tf_dict.get(doc, {})  # builds out nested dic for every doc in corpus
+        total_doc_num += 1   # keeps track of # of docs in the corpus
         for term in query:
             in_doc = False
             term_count = 0
@@ -23,15 +23,19 @@ def build_dicts(query, corpus):
                     term_count += 1
             tf_dict[doc][term] = term_count
             if in_doc:
-                idf_dict[term] = idf_dict.get(term,0)+1  #number of docs that contain term
+                idf_dict[term] = idf_dict.get(term,0)+1  # number of docs that contain term
 
     return idf_dict, tf_dict, total_doc_num
 
 def find_rel_score(idf_dict, tf_dict, total_doc_num, query):
-    rel_score = None
+    rel_score = 0
     for term in query:
         num_doc_with_term = idf_dict[term]
         idf = math.log(total_doc_num/num_doc_with_term)
         for doc, inner_dict in tf_dict.iteritems():
-            tf = inner_dict[term] # returns number of times a term appears in that particular doc
+            tf = inner_dict[term]  # returns number of times a term appears in that particular doc
+            term_score = idf * tf
+            rel_score += rel_score
+    return rel_score
+
 
