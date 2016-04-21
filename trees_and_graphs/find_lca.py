c7046856be17ef9c6ba5a3eb7692fc7d98867fd0
karenwsit <karenwsit@gmail.com>
2016-04-21 14:19:33 -0700

    added doctests. all tests passed

--- trees_and_graphs/find_lca.py
+++ trees_and_graphs/find_lca.py
@@ -11,25 +11,33 @@ class Node:
         self.right = None
 
 def find_lca_bst(node, n1, n2):
+    """
+    >>> find_lca_bst(node, 10, 14)
+    12
+    >>> find_lca_bst(node, 14, 8)
+    8
+    >>> find_lca_bst(node, 10, 22)
+    20
+    """
     #base case
     if node is None:
         return None
 
-    if n1 > n2:
-        n1 = n2
-        n2 = n1
-
     if n1 < node.data and n2 < node.data:
-        find_lca_bst(node.left, n1, n2)
-    elif n > node.data and n2 > node.data:
-        find_lca_bst(node.right, n1, n2)
-    else:
-        return node
+        return find_lca_bst(node.left, n1, n2)
+    if n1 > node.data and n2 > node.data:
+        return find_lca_bst(node.right, n1, n2)
+    return node.data
 
-root = Node(20)
-root.left = Node(8)
-root.right = Node(22)
-root.left.left = Node(4)
-root.left.right = Node(12)
-root.left.right.left = Node(10)
-root.left.right.right = Node(14)
+node = Node(20)
+node.left = Node(8)
+node.right = Node(22)
+node.left.left = Node(4)
+node.left.right = Node(12)
+node.left.right.left = Node(10)
+node.left.right.right = Node(14)
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
